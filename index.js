4c572c4e253faed1aee7eadf503451049e4f510b
BohdanPotapchuk <baaaagdan4444@gmail.com>
2022-09-07 15:03:43 +0300

    run a new project

---
+++ index.js
@@ -0,0 +1,11 @@
+const menu1 = document.querySelector('#menu1')
+const about_us = document.querySelector('#about_us')
+const rules = document.querySelector('#rules')
+const contact = document.querySelector('#contact')
+const etc = document.querySelector('#etc')
+
+const menuDisplay = document.querySelector('.main_display')
+const about_usDisplay = document.querySelector('.about_us_display')
+const rulesDisplay = document.querySelector('.rules_display')
+const contactDisplay = document.querySelector('.contact_display')
+const etcDisplay = document.querySelector('.etc_display')
